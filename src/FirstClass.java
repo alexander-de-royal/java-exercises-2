f151ddb79318e47a724f492b6b03546ad8f61490
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-29 15:05:50 +0545

    day 2 completed

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,34 @@
-//public class FirstClass {
-//    public static void main(String[] args) {
-//        System.out.println("Hello World");
-//    }
-//}
+//  Day 2
+
+    /*
+    *  Where to write java code:
+    *   1) Editors:
+    *           - BlueJ
+    *           - doctor Java
+    *           - SublimeText
+    *           - Notepad
+    *   2) IDEs
+    *           - Eclipse
+    *           - Netbeans
+    *           - IntelliJ
+    *           - STS
+    *           - VS Code
+    *
+    *   3) Keyword (Access Modifiers)
+    *           - public => access everywhere
+    *           - protected => access from class and package
+    *           - default(package private)
+    *           - private => class only
+    *           - class
+    *   4) Identifiers
+    *           - name of class
+    *
+    *   5) Delimiters
+    *           - ;
+    *           - []
+    *   6) static - memory management
+    *
+    *   7) main methods runs the compiler
+    *   8) JVM runs main methods
+    *   9) main method always have void
+    * */
--- test.java
+++ test.java
@@ -1,5 +1,10 @@
+import javax.swing.JOptionPane;
+
 class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java ");
+//        System.out.println("Hello: " + args[0]);
+
+        JOptionPane.showMessageDialog(null, "Welcome");
     }
 }

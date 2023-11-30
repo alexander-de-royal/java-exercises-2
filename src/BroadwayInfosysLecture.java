bde0bdccc29ea1f8db0976ec970fe685a7bc9397
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-30 14:42:14 +0545

    day 3 lecture completed

---
+++ src/BroadwayInfosysLecture.java
@@ -0,0 +1,98 @@
+//  Day 2
+
+    /*
+    *  Where to write java code:
+    *   1) Editors:
+    *           - BlueJ
+    *           - doctor Java
+    *           - SublimeText
+    *           - Notepad
+    *   2) IDEs
+    *           - Eclipse
+    *           - Netbeans
+    *           - IntelliJ
+    *           - STS
+    *           - VS Code
+    *
+    *   3) Keyword (Access Modifiers)
+    *           - public => access everywhere
+    *           - protected => access from class and package
+    *           - default(package private)
+    *           - private => class only
+    *           - class
+    *   4) Identifiers
+    *           - name of class
+    *
+    *   5) Delimiters
+    *           - ;
+    *           - []
+    *   6) static - memory management
+    *
+    *   7) main methods runs the compiler
+    *   8) JVM runs main methods
+    *   9) main method always have void
+    *
+    *   Java is a strongly typed language
+    * */
+
+//  Day 3
+
+    /*
+    *  -- Data Type --
+    *
+    *   # proper data representation
+    *   # proper memory allocation
+    *   # proper operation to be performed
+    *
+    *   # Types:
+    *
+    *   1) Primitive:
+    *       - byte
+    *       - short
+    *       - int
+    *       - long
+    *       - float
+    *       - double
+    *       - char
+    *       - boolean
+    *
+    *   2) Non-Primitive
+    *       - String
+    *       - Array
+    *       - Classes
+    *       - Collection
+    *
+    *
+    ------ Wrapper Classes ----------
+    # every primitive data type has respective class is known as wrapper class
+
+    Primitive - Wrapper
+    byte - Byte
+    short - Short
+    int - Integer
+    long - Long
+    float - Float
+    double - Double
+    char - Character
+    boolean - Boolean
+
+    Wrapper is mostly used in conversion
+    *
+    * ------------- Auto Boxing ------------
+    *   # process of conversion of primitive type to wrapper type
+    *
+    *   - e.g.
+    *       int x = 3443;
+    *       Integer a = x; // auto-boxing
+    *
+    * ------------ Auto Unboxing ------------
+    *   # process of conversion of wrapper type to primitive type
+    *
+    *   - e.g.
+    *       Double k = 344.43
+    *       double p = k; // auto-unboxing
+    *
+    * - BigInteger -
+    * - BigDecimal - => Refer to class BigNumbers
+    *
+    */
---
+++ src/WrapperTest.java
@@ -0,0 +1,21 @@
+package src;
+
+public class WrapperTest {
+
+    /*
+    ------ Wrapper Classes ----------
+    # every primitive data type has respective class is known as wrapper class
+
+    Primitive - Wrapper
+    byte - Byte
+    short - Short
+    int - Integer
+    long - Long
+    float - Float
+    double - Double
+    char - Character
+    boolean - Boolean
+
+    Wrapper is mostly used in conversion
+    */
+}

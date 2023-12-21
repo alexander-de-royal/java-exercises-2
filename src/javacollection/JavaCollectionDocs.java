15baf42e891e2bd5f31abf95bf94a2fd671b63cd
Alexander Royal <alexander.de.royal@gmail.com>
2023-12-21 14:40:51 +0545

    java collection lecture completed

---
+++ src/javacollection/JavaCollectionDocs.java
@@ -0,0 +1,23 @@
+package src.javacollection;
+
+public class JavaCollectionDocs {
+
+    /*
+    * ------------------- Java Collection / Collection Framework --------------------
+    *  # use to store and processing group/collection of dynamic data/objects.
+    *
+    *   1) List: dynamic array
+    *       a) ArrayList: Processing slow, better to use for data storage
+    *       b) LinkedList: Processing fast, better to use for data processing
+    *
+    *   2) Set: collection of unique data
+    *       a) HashSet: unique data only
+    *       b) TreeSet: unique + sorted
+    *       c) LinkedHashSet: unique + insertion in order
+    *
+    *   3) Map: data manage in key, value pair
+    *       a) HashMap: <k,v> pair
+    *       b) TreeMap: <k,v> + sorted data
+     *       c) LinkedHashMap: <k,v> + insertion in order
+     * */
+}
